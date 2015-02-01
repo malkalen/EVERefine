c053fc4977241024d3cdd05f338ac39749544b8b
malkalen <alan.mccahon@googlemail.com>
2015-02-01 00:15:29 +0000

    gets assets if a keyId vCode and C

--- EVERefine/Form1.cs
+++ EVERefine/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using EveAI.Live;
+using EveAI.Live.Utility;
 
 namespace EVERefine
 {
@@ -15,7 +11,51 @@ namespace EVERefine
         public Form1()
         {
             InitializeComponent();
-            // do stuff
+        }
+
+        private void GetAssetsBtn_Click(object sender, EventArgs e)
+        {
+            GetAssetsTask getAssetsTask = new GetAssetsTask(Convert.ToInt64(KeyIDTxt.Text), vCodeTxt.Text, Convert.ToInt64(characterTxt.Text));
+            getAssetsTask.GetAssets();
+        }
+
+        private void vCodeTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class GetAssetsTask
+    {
+        private readonly long _keyId;
+        private readonly string _vCode;
+        private readonly long _character;
+
+        public GetAssetsTask(long keyId, string vCode, long character)
+        {
+            _keyId = keyId;
+            _vCode = vCode;
+            _character = character;
+        }
+
+        public void GetAssets()
+        {
+            try
+            {
+                EveApi api = new EveApi("EVERefine", _keyId, _vCode, _character);
+                List<Asset> assets = api.GetCharacterAssets();
+                foreach (var asset in assets)
+                {
+                    asset.LocationID = 0;
+                }
+
+            }
+            catch (Exception)
+            {
+                
+                throw;
+            }
+
         }
     }
 }
